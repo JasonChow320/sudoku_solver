e72220f965fde042417b92f4f2a6333bd195d3e1
Jason Chow <jasonchow9168@gmail.com>
2023-02-03 10:58:08 -0500

    [feat] add include guards and clang-format

--- sudoku_solver.h
+++ sudoku_solver.h
@@ -1,11 +1,17 @@
+#ifndef SUDOKU_SOLVER
+#define SUDOKU_SOLVER
+
 #define BOARD_SIZE 9
 #define BOX_SIZE 3
 
-typedef struct board{
+typedef struct board {
+
     int board[BOARD_SIZE][BOARD_SIZE];
 } board_t;
 
-void display_board(board_t* board);
-board_t* initalize_board(void);
-int solve(board_t* board, int row, int col);
-int find_missing_numbers(int row, int col, board_t* board, int* BUF, int size);
+void display_board(board_t *board);
+board_t *initalize_board(void);
+int solve(board_t *board, int row, int col);
+int find_missing_numbers(int row, int col, board_t *board, int *BUF, int size);
+
+#endif
